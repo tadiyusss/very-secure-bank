9a1cdb43f8c54c74d7e68a0388d7e9218a4db4d2
Thaddeus Toledo <toledothaddeusf@gmail.com>
2025-02-26 13:12:48 +0800

    debug option in environment

--- very_secure_bank/settings.py
+++ very_secure_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z#qus0w7@57m(0n&cq_me4-tc-^yb^1km9q74y**k7-1hhpngg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
